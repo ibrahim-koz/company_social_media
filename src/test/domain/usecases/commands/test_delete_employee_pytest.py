52f69d09afc8da1cccd05f251e8e20a0d3b32d09
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2022-08-25 23:47:49 +0300

    Completed DeleteEmployee

---
+++ src/test/domain/usecases/commands/test_delete_employee_pytest.py
@@ -0,0 +1,33 @@
+import pytest
+
+from src.app.domain.exceptions.employee_not_found_exception import EmployeeNotFoundException
+from src.app.domain.requests.delete_employee_request import DeleteEmployeeRequest
+from src.app.domain.usecases.commands.delete_employee import DeleteEmployee
+from src.app.domain.usecases.commands.delete_entry import DeleteEntry
+
+
+@pytest.fixture
+def delete_employee_fixture(setup_mock_data):
+    company_repository, employee_repository, entry_repository = setup_mock_data
+    delete_entry = DeleteEntry(employee_repository, entry_repository)
+    delete_employee = DeleteEmployee(company_repository, employee_repository, delete_entry)
+    return delete_employee, company_repository, employee_repository
+
+
+def test_handle(delete_employee_fixture):
+    delete_employee, company_repository, employee_repository = delete_employee_fixture
+    employee_id = 1
+    delete_employee_request = DeleteEmployeeRequest(employee_id)
+    employee = employee_repository.get_employee_by_id(employee_id)
+
+    delete_employee.handle(delete_employee_request)
+    assert not company_repository.get_company_by_id(employee.company_id).has_employee(employee_id)
+
+
+def test_handle_not_found_employee(delete_employee_fixture):
+    delete_employee, _, _ = delete_employee_fixture
+    employee_id = 0
+    delete_employee_request = DeleteEmployeeRequest(employee_id)
+
+    with pytest.raises(EmployeeNotFoundException):
+        delete_employee.handle(delete_employee_request)
--- src/test/domain/usecases/commands/test_delete_entry_pytest.py
+++ src/test/domain/usecases/commands/test_delete_entry_pytest.py
@@ -22,3 +22,12 @@ def test_handle(delete_entry_fixture):
     assert not employee_repository.get_employee_by_id(entry.employee_id).has_entry(entry_id)
     with pytest.raises(EntryNotFoundException):
         entry_repository.get_entry_by_id(entry_id)
+
+
+def test_handle_not_found_entry(delete_entry_fixture):
+    delete_entry, _, _ = delete_entry_fixture
+    entry_id = 0
+    delete_entry_request = DeleteEntryRequest(entry_id)
+
+    with pytest.raises(EntryNotFoundException):
+        delete_entry.handle(delete_entry_request)
